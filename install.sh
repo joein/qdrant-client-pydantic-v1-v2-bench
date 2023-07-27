db5ea5166a771bfc56731713de0ca3c57d7fd0f4
George Panchuk <george.panchuk@qdrant.tech>
2023-07-27 13:57:12 +0300

    new: update pydantic v2, install qdrant-client from pypi

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ python3 -m venv venv
 source venv/bin/activate
 echo 'Installing dependencies'
 pip3 install -U pip seaborn
-pip3 install git+https://github.com/qdrant/qdrant-client.git@master
+pip3 install qdrant-client==1.3.2
 wget https://raw.githubusercontent.com/h4pZ/rose-pine-matplotlib/main/themes/rose-pine-moon.mplstyle -P styles
 mkdir results
--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ docker run --rm -d -p 6333:6333 -p 6334:6334 --name qdrant qdrant/qdrant:latest
 pip3 install pydantic==1.10.8 -q
 echo 'Running pydantic v1 (1.10.8)'
 python3 -m run
-pip3 install pydantic==2.0.3 -q
-echo 'Running pydantic v2 (2.0.3)'
+pip3 install pydantic==2.1.1 -q
+echo 'Running pydantic v2 (2.1.1)'
 python3 -m run
 echo 'Building plots'
 python3 -m plots
